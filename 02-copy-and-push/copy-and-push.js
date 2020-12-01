482517055d3b20d4c72b28d55e93375dd2ff18a6
Jena Boehm <boehmjena@gmail.com>
2020-12-01 15:45:31 -0800

    push new value to array and return original array

--- 02-copy-and-push/copy-and-push.js
+++ 02-copy-and-push/copy-and-push.js
@@ -1,5 +1,8 @@
-const copyAndPush = () => {
+// create a function that returns a new array with all the items in the original array and a new item pushed to the end.
 
+const copyAndPush = (arr, newValue) => {
+    arr.push(newValue)
+    return arr;
 };
 
 module.exports = { copyAndPush };
--- 02-copy-and-push/copy-and-push.test.js
+++ 02-copy-and-push/copy-and-push.test.js
@@ -1,9 +1,15 @@
+//1. create your function, use `push` to add item to array and return array
+// green, commit
+
 const { copyAndPush } = require('./copy-and-push.js');
 
 describe('copyAndPush', () => {
     it('returns a new array with the original array and new item pushed to the end ', () => {
-        const numbers = [1, 2, 3]; 
+        const arr = [1, 2, 3]; 
+        const newValue = 4;
+
+        const newArr = copyAndPush(arr, newValue);
         
-        expect(numbers).toEqual([1, 2, 3]);
+        expect(newArr).toEqual([1, 2, 3, 4]);
     });
 });
