98eb19794d01afadd602ad115ed8eadc169c13b3
Jena Boehm <boehmjena@gmail.com>
2020-12-01 14:59:57 -0800

    refactor, final passing tests

--- 01-get-name/get-name.js
+++ 01-get-name/get-name.js
@@ -1,23 +1,8 @@
 // create a function that returns the name property of an object
 
-const spot = { 
-    name: 'spot', 
-    age: 5, 
-    weight: '20 lbs' 
-}
-
-const character = { 
-    _id: '5cf5679a915ecad153ab68c9', 
-    name: 'Aang' 
-};
-
 const getName = (obj) => {
     const { name } = obj;
     return name;
-}
+};
 
-console.log(getName(spot), getName(character));
-
-module.exports = {
-    getName
-}
+module.exports = { getName };
--- 01-get-name/get-name.test.js
+++ 01-get-name/get-name.test.js
@@ -1,25 +1,27 @@
-// refactor, destructure to get the objects name
+// refactor, destructure function parameters and return name
 // tests stay green, commit
 
 const { getName } = require('./get-name.js');
 
-test('returns the name property of an object', () => {
-    const spot = { 
-        name: 'spot', 
-        age: 5, 
-        weight: '20 lbs' 
-    }
-    const name = getName(spot)
-    
-    expect(name).toEqual('spot');
-});
+describe('getName', () => {
+    it('returns the name property of an object', () => {
+        const spot = { 
+            name: 'spot', 
+            age: 5, 
+            weight: '20 lbs' 
+        }
+        const name = getName(spot)
+        
+        expect(name).toEqual('spot');
+    });
 
-test('returns the name property of an object', () => {
-    const character = { 
-        _id: '5cf5679a915ecad153ab68c9', 
-        name: 'Aang' 
-    };
-    const name = getName(character)
-    
-    expect(name).toEqual('Aang');
+    it('returns the name property of any object', () => {
+        const character = { 
+            _id: '5cf5679a915ecad153ab68c9', 
+            name: 'Aang' 
+        };
+        const name = getName(character)
+        
+        expect(name).toEqual('Aang');
+    });
 });
