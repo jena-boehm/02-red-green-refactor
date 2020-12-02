7b5118f32cb82c431e9deedf4785289c04405440
Jena Boehm <boehmjena@gmail.com>
2020-12-01 16:19:48 -0800

    use for loop to capitalize all strings and filter out any that start with 'f'

--- 03-capitalize-and-filter/capitalize-and-filter.js
+++ 03-capitalize-and-filter/capitalize-and-filter.js
@@ -1,5 +1,17 @@
-const capitalizeAndFilter = () => {
+// create a function that takes an array of strings capitalize all strings and filter out any string that starts with the letter f.
 
+const capitalizeAndFilter = (arr) => {
+    const newArr = []; 
+
+    for(let i = 0; i < arr.length; i++) {
+        const item = arr[i];
+        const uppercaseItem =  item.toUpperCase();
+
+        if(uppercaseItem.charAt(0) !== 'F') {
+            newArr.push(uppercaseItem);
+        }
+    }
+    return newArr;
 };
 
 module.exports = { capitalizeAndFilter };
--- 03-capitalize-and-filter/capitalize-and-filter.test.js
+++ 03-capitalize-and-filter/capitalize-and-filter.test.js
@@ -3,7 +3,9 @@ const { capitalizeAndFilter } = require('./capitalize-and-filter.js');
 describe('capitalizeAndFilter', () => {
     it('takes an array of strings, capitalizes all strings, and filters out any string that starts with the letter f ', () => {
         const array = ['spot', 'rover', 'bingo', 'fred']
+
+        const newArr = capitalizeAndFilter(array);
         
-        expect(array).toEqual(['spot', 'rover', 'bingo', 'fred']);
+        expect(newArr).toEqual(['SPOT', 'ROVER', 'BINGO']);
     });
 });
